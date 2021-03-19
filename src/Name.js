ad97b92660dd0c408d70837f90e9d309d691f27f
Amanuel <weditedi25@gmail.com>
2021-03-19 15:02:05 +0000

    comment added

--- src/Name.js
+++ src/Name.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Name = (props) => {
+  //empty class to be filled based on the condition
   let cssClass = "";
   props.babyName.sex === "f" ? (cssClass = "female") : (cssClass = "male");
   return <span className={cssClass}>{props.babyName.name}</span>;
